cdc1e1bf418236ec7915d9960131f60980273429
Shady <shady@shadyproject.net>
2014-02-28 21:19:01 -0800

    Initial Stuff

---
+++ TravelGuide/SPTGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SPTGAppDelegate.h
+//  TravelGuide
+//
+//  Created by Christopher Martin on 2/28/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPTGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravelGuide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TravelGuide
+//
+//  Created by Christopher Martin on 2/28/14.
+//  Copyright (c) 2014 shadyproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SPTGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPTGAppDelegate class]));
+    }
+}
